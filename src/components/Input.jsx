2880bade042a68458c4e67cfb6f04521989e2ec9
teejay327 <mrenergy@hotmail.com>
2024-10-04 23:42:08 +1000

    Added styling to buttons and input elements

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,16 @@
 const Input = ({ label, textarea, ...props }) => {
-   return <p>
-    <label>{ label }</label>
-    {textarea ? <textarea  { ...props } /> : <input { ...props }/>}
+const classes = "w-full p-1 border-b-2 rounded border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
+   return <p className="flex flex-col gap-1 my-4">
+    <label className="text-sm font-bold uppercase text-stone-500">{ label }</label>
+    {textarea ? (
+      <textarea 
+         className={ classes } 
+         { ...props } 
+      />
+    ) : (
+      <input className={classes} { ...props }/>
+    )}
    </p>
 }
 
