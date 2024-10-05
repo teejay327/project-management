076fb3d2c29f80d1b808bc8b46b8438b41af4466
teejay327 <mrenergy@hotmail.com>
2024-10-05 23:10:24 +1000

    Added Tailwinds css to Button and NewProjects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ProjectsSidebar  from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjecteSelected.jsx";
 
 const App = () => {
 
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,13 +1,14 @@
+import Button from './Button.jsx';
+
 const ProjectsSidebar = () => {
 
   return <aside className="w-1/3 px-8 py-16 bg-stone-900
    text-stone-300 md:w-72 rounded-r-xl" aside>
     <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
     <div>
-      <button className="px-4 py-2 text-xs md:text-base rounded-md
-        bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+      <Button>
         + Add project
-      </button>
+      </Button>
     </div>
     <ul>
 
